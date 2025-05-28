2d5b3e3190956e623dc1902c2bd0deacb2a4235b
guanhai <q2274799107@126.com>
2025-05-28 23:13:50 +0800

    chore: 添加全局样式 reset.scss

--- vite.config.ts
+++ vite.config.ts
@@ -11,47 +11,59 @@ import { defineConfig } from 'vite'
 // import WebfontDownload from 'vite-plugin-webfont-dl'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    VueRouter({
-      routesFolder: [
-        {
-          src: 'src/pages',
-          path: '',
-          exclude: '**/components/**',
-          extensions: ['.vue'],
-        },
-      ],
-      dts: 'types/plugins/auto-router.d.ts',
-    }),
-    AutoImport({
-      imports: ['vue', 'vue-router', 'pinia', VueRouterAutoImports],
-      resolvers: [ElementPlusResolver()],
-      dts: 'types/plugins/auto-import.d.ts',
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-      dts: 'types/plugins/auto-components.d.ts',
-    }),
-    // WebfontDownload([
-    //   'https://fonts.bunny.net/css?family=noto-sans-sc:100,200,300,400,500,600,700,800,900|zcool-kuaile:400',
-    // ]),
-  ],
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './'),
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ command, mode }) => {
+
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      VueRouter({
+        routesFolder: [
+          {
+            src: 'src/pages',
+            path: '',
+            exclude: '**/components/**',
+            extensions: ['.vue'],
+          },
+        ],
+        dts: 'types/plugins/auto-router.d.ts',
+      }),
+      AutoImport({
+        imports: ['vue', 'vue-router', 'pinia', VueRouterAutoImports],
+        resolvers: [ElementPlusResolver()],
+        dts: 'types/plugins/auto-import.d.ts',
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+        dts: 'types/plugins/auto-components.d.ts',
+      }),
+      // WebfontDownload([
+      //   'https://fonts.bunny.net/css?family=noto-sans-sc:100,200,300,400,500,600,700,800,900|zcool-kuaile:400',
+      // ]),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/styles/reset.scss" as *;`,
+          devSourcemap: true
+        }
+      }
     },
-    extensions: ['.ts', '.js', '.mjs', '.jsx', '.tsx', '.json', '.vue'],
-  },
-  server: {
-    port: 5173,
-    strictPort: true,
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
-  },
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './'),
+        '@': path.resolve(__dirname, './src'),
+      },
+      extensions: ['.ts', '.js', '.mjs', '.jsx', '.tsx', '.json', '.vue'],
+    },
+    server: {
+      port: 5173,
+      strictPort: true,
+      open: true,
+    },
+    build: {
+      outDir: 'dist',
+    },
+    cacheDir: 'dist/.cache'
+  }
 })
